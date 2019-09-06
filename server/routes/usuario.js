7b78503cd1728db65b6c0dc68ab618467ea90ed6
Rossana <rossa42@gmail.com>
2019-09-06 11:52:45 -0400

    rebuild

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Usuario = require('../models/usuario');
 const app = express();
-const bcrypt = require('bcryptJS');
+const bcrypt = require('bcryptjs');
 const _ = require('underscore');
 
 app.get('/usuario', function(req, res) {
